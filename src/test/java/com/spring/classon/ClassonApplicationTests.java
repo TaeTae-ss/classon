3a68ec5b2ea8182cb5ab470edfebe5e426ff3224
TaeTae-ss <dnxotlr2@gmail.com>
2026-09-17 10:36:17 +0900

    프로젝트 생성

---
+++ src/test/java/com/spring/classon/ClassonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.classon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
