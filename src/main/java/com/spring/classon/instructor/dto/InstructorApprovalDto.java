b83535ed264a7f4af7057a011fc4e7891d2154d6
AnaisKL <anaiskl@gmail.com>
2026-09-22 00:32:13 +0900

    강사 신청 승인/거절 Dto

---
+++ src/main/java/com/spring/classon/instructor/dto/InstructorApprovalDto.java
@@ -0,0 +1,12 @@
+package com.spring.classon.instructor.dto;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InstructorApprovalDto {
+
+    private String reqStatus;
+    private String rejReason;
+}
