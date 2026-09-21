2e82f31af8401b010e9bcf75bcc3f5739748555f
AnaisKL <anaiskl@gmail.com>
2026-09-22 00:46:37 +0900

    강사 신청 승인/거절 Repository 수정

--- src/main/java/com/spring/classon/instructor/repository/InstructorRejectionRepository.java
+++ src/main/java/com/spring/classon/instructor/repository/InstructorRejectionRepository.java
@@ -3,5 +3,9 @@ package com.spring.classon.instructor.repository;
 import com.spring.classon.instructor.entity.InstructorRejection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InstructorRejectionRepository extends JpaRepository<InstructorRejection, Long> {
+
+    Optional<InstructorRejection> findByReqNo(Long reqNo);
 }
