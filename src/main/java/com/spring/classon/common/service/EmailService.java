9405a05f68b9a6fdbe86a489232a0125d1cf09e2
AnaisKL <anaiskl0618@gmail.com>
2026-09-22 17:25:06 +0900

    이메일 인증 관련 인증번호 + 인증번호 만료 시간 + 인증 번호 저장

--- src/main/java/com/spring/classon/common/service/EmailService.java
+++ src/main/java/com/spring/classon/common/service/EmailService.java
@@ -4,4 +4,7 @@ public interface EmailService {
 
     // 인증 메일 발송
     void sendEmail(String email, String authCode);
+
+    // 인증번호 확인
+    boolean verifyEmail(String email, String authCode);
 }
--- src/main/java/com/spring/classon/common/service/EmailServiceImpl.java
+++ src/main/java/com/spring/classon/common/service/EmailServiceImpl.java
@@ -5,15 +5,24 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender mailSender;
+    private final Map<String, AuthCodeInfo> authCodeMap = new ConcurrentHashMap<>();
+
 
     // 인증 메일 발송
     @Override
     public void sendEmail(String email, String authCode) {
+
+        // 인증번호 저장
+        saveAuthCode(email, authCode);
 
         SimpleMailMessage message = new SimpleMailMessage();
 
@@ -24,5 +33,45 @@ public class EmailServiceImpl implements EmailService {
         );
 
         mailSender.send(message);
+    }
+
+    // 인증번호 저장
+    public void saveAuthCode(String email, String authCode) {
+
+        authCodeMap.put(
+                email,
+                new AuthCodeInfo(
+                        authCode,
+                        LocalDateTime.now().plusMinutes(5)
+                )
+        );
+    }
+
+    // 인증번호 확인
+    @Override
+    public boolean verifyEmail(String email, String authCode) {
+
+        AuthCodeInfo info = authCodeMap.get(email);
+
+        // 인증번호가 없는 경우
+        if (info == null) {
+            return false;
+        }
+
+        // 인증번호가 만료된 경우
+        if (LocalDateTime.now().isAfter(info.getExpiresAt())) {
+            authCodeMap.remove(email);
+            return false;
+        }
+
+        // 인증번호가 다른 경우
+        if (!info.getAuthCode().equals(authCode)) {
+            return false;
+        }
+
+        // 인증 성공 후 인증번호 삭제
+        authCodeMap.remove(email);
+
+        return true;
     }
 }
